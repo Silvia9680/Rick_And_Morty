c4d3b71d522905a3d0b47c590141233cd7f28fa4
Silvia9680 <silviacarolina9680@gmail.com>
2023-09-07 09:09:23 -0300

    Card creada

--- src/components/ListCharacters.vue
+++ src/components/ListCharacters.vue
@@ -2,7 +2,7 @@
   <section>
     <div class="characters">
         <div class="characters__item" v-for="character in characters" :key="character.id">
-            <h2>{{character.name}}</h2>
+            <CardCharacter :character="character"/>
         </div>
     </div>
   </section>
@@ -11,20 +11,24 @@
 <script>
 import { computed, onMounted } from 'vue'
 import { useStore } from 'vuex'
+import CardCharacter from '@/components/CardCharacter'
 export default{
-   setup(){
-      const store = useStore()
-      const characters = computed(() =>{
-        return store.state.charactersFilter
-      })
-      onMounted(() => {
-        store.dispatch('getCharacters')
-      })
-
-      return {
-        characters
-      }
-   }
+  components: { 
+    CardCharacter
+    },
+      setup() {
+        const store = useStore();
+        const characters = computed(() => {
+            return store.state.charactersFilter;
+        });
+        onMounted(() => {
+            store.dispatch('getCharacters');
+        })
+        return {
+            characters
+        };
+    }
+    
 }
 </script>
 
